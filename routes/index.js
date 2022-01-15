7987a1379826d2f8cde387f3348e767e16232ff6
TonyPurple <tony.vanoni@outlook.com>
2022-01-15 13:38:20 -0800

    submitting base requirements

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
+const Todo = require('../data/todos')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Unit 2 Assessment' });
+    res.render('index', {
+        title: 'Unit 2 Assessment',
+        todos: Todo.getAll()
+    });
 });
+
+router.post("/", function(req, res) {
+    console.log(req.body.newtodo)
+    let newObj = {
+        todo: req.body.newtodo,
+        done: false,
+    }
+    let newToDo = Todo.getAll()
+    newToDo.push(newObj)
+
+
+    res.render("index", {
+        title: 'Unit 2 Assessment',
+        todos: Todo.getAll()
+    });
+});
+
 
 module.exports = router;
